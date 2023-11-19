3c3ee352db27fff12b4ac533dff8534bbbd1db7c
fabioaraujob2b <fabioaraujob2b@gmail.com>
2023-11-19 20:16:23 -0300

    Me deu sono

--- index.php
+++ index.php
@@ -1,44 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include 'view/estrutura/header.php';?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="public/css/style.css">
-    <title>ReserViva</title>
-</head>
-
-<body>
-    <nav class="navbar navbar-expand navbar-light bg-light d-flex justify-content-around">
-        <div class="container">
-            <a class="navbar-brand" href="#">ReserViva</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav mx-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Quartos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Reservas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contato</a>
-                    </li>
-                </ul>
-            </div>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</body>
-
-</html>
+oi
+<?php include 'view/estrutura/footer.php';?>
